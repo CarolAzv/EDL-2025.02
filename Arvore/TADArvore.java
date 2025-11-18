d64cbd9827113314e72ba05e8bdaf27c6b11096d
Carol <caroldazevedo52@gmail.com>
2025-11-18 13:33:50 -0300

    Refactor TADArvore to use public interface

--- Arvore/TADArvore.java
+++ Arvore/TADArvore.java
@@ -1,20 +1,22 @@
-//Métodos genéricos:
-integer size();
-integer height();
-boolean isEmpty();
-Iterator elements();
-Iterator nos();
-  
-//Métodos de acesso:
-No root();
-No parent(No);
-Iterator children(No);
-  
-//Métodos de consulta:
-boolean isInternal(No);
-boolean isExternal(No);
-boolean isRoot(No);
-integer depth(No);
-                
-//Métodos de atualização:
-Object replace(No, o);
+Public interface TADArvore{
+  //Métodos genéricos:
+  integer size();
+  integer height();
+  boolean isEmpty();
+  Iterator elements();
+  Iterator nos();
+    
+  //Métodos de acesso:
+  No root();
+  No parent(No);
+  Iterator children(No);
+    
+  //Métodos de consulta:
+  boolean isInternal(No);
+  boolean isExternal(No);
+  boolean isRoot(No);
+  integer depth(No);
+                  
+  //Métodos de atualização:
+  Object replace(No, o);
+}
