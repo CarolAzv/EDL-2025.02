b2e97a58c4adfd17eefaf2f5f5ecae6ed2384c6b
Carol <caroldazevedo52@gmail.com>
2025-11-18 13:29:09 -0300

    Implement NoTriplo class for binary tree nodes

--- Arvore/BinariaPesquisa/NoTriplo.java
+++ Arvore/BinariaPesquisa/NoTriplo.java
@@ -1,1 +1,56 @@
+public class NoTriplo extends Object{
+    private Object elemento;
+    private No pai;
+    private No esquerda;
+    private No direita;
 
+    public NoTriplo(Object o){
+        elemento = o;
+        pai = null
+        esquerda = null;
+        direita = null;
+    }
+
+  
+    public void setElemento(Object o){
+        this.elemento = o;
+    }
+
+    public Object getElemento(){
+        return elemento;
+    }
+
+  
+    public void setEsquerda(No o){
+        this.esquerda = o;  
+    }
+
+    public No getEsquerda(){
+        if(esquerda==null)
+            return null;
+        return esquerda;
+    }
+
+  
+  public void setDireita(No o){
+        this.direita = o;  
+    }
+
+    public No getDireita(){
+        if(direita==null)
+            return null;
+        return direita;
+    }
+
+  
+    public void setPai(No o){
+        this.pai = o;  
+    }
+
+    public No getPai(){
+        if(pai==null)
+            return null;
+        return pai;
+    }
+
+}
