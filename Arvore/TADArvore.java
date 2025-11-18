874483566c520a48a5f757534117a47f9f11b172
Carol <caroldazevedo52@gmail.com>
2025-11-18 13:33:04 -0300

    Create TADArvore.java

---
+++ Arvore/TADArvore.java
@@ -0,0 +1,20 @@
+//Métodos genéricos:
+integer size();
+integer height();
+boolean isEmpty();
+Iterator elements();
+Iterator nos();
+  
+//Métodos de acesso:
+No root();
+No parent(No);
+Iterator children(No);
+  
+//Métodos de consulta:
+boolean isInternal(No);
+boolean isExternal(No);
+boolean isRoot(No);
+integer depth(No);
+                
+//Métodos de atualização:
+Object replace(No, o);
