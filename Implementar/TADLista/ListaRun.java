a60f9eee4adc53a76e7700b239e42bb98b95f0dc
Carol <caroldazevedo52@gmail.com>
2025-11-19 22:19:33 -0300

    Refactor ListaRun to utilize Lista class methods
    Refactored ListaRun to use Lista methods for list operations.

--- Implementar/TADLista/ListaRun.java
+++ Implementar/TADLista/ListaRun.java
@@ -1,63 +1,62 @@
-public class ListaRun extends Object{
-  private static No s;
-  private static No f;
+public class ListaRun{
+  private static Lista ll;
   public static void main(String[] args){
+    ll = new Lista();
     No temp1;
     No temp2;
-    s = new No("Começo");
-    f = new No("Fim");
-    s.setProximo(f);
-    f.setAnterior(s);
+    int tam;
 
     
     //print
     System.out.println("Lista:");
-    temp1 = s;
-    while(temp1 != f){
+    temp1 = ll.first();
+    tam = ll.size();
+    for(int i=0; i<tam; i++){
             System.out.println(temp1.getElemento());
-            temp1 = temp1.getProximo();
+            temp1 = ll.after(temp1);
         }
     System.out.println(f.getElemento());
 
     //Adicionar
-    System.out.println("Adicionando 'Cachorro'");
-    temp1 = f.getAnterior();
-    temp2 = new No("Cachorro");
-    (temp2).setAnterior(temp1);
-    (temp2).setProximo(f);
-    (temp1).setProximo(temp2);
-    f.setAnterior(temp2);
-
-    System.out.println("Adicionando 'Gato'");
-    temp1 = f.getAnterior();
-    temp2 = new No("Gato");
-    (temp2).setAnterior(temp1);
-    (temp2).setProximo(f);
-    (temp1).setProximo(temp2);
-    f.setAnterior(temp2);
+    System.out.println("Adicionando 'Cachorro' ao ultimo");
+    ll.insertLast("Cachorro");
 
     //print
     System.out.println("Lista:");
-    temp1 = s;
-    while(temp1 != f){
+    temp1 = ll.first();
+    tam = ll.size();
+    for(int i=0; i<tam; i++){
             System.out.println(temp1.getElemento());
-            temp1 = temp1.getProximo();
+            temp1 = ll.after(temp1);
         }
     System.out.println(f.getElemento());
 
-    //Remover
-    System.out.println("Removendo último elemento");
-    temp1 = f.getAnterior();
-    No prev = temp1.getAnterior();
-    prev.setProximo(f);
-    f.setAnterior(prev);
+    //Adicionar
+    System.out.println("Adicionando 'Gato' no primeiro");
+    ll.insertFirst("Gato");
 
     //print
     System.out.println("Lista:");
-    temp1 = s;
-    while(temp1 != f){
+    temp1 = ll.first();
+    tam = ll.size();
+    for(int i=0; i<tam; i++){
             System.out.println(temp1.getElemento());
-            temp1 = temp1.getProximo();
+            temp1 = ll.after(temp1);
+        }
+    System.out.println(f.getElemento());
+
+    //Remover e Adicionar
+    System.out.println("Removendo 'Cachorro' e trocando 'Gato' por 'Coelho");
+    ll.remove((ll.first()).after());
+    ll.replaceElement((ll.last()).before());
+
+    //print
+    System.out.println("Lista:");
+    temp1 = ll.first();
+    tam = ll.size();
+    for(int i=0; i<tam; i++){
+            System.out.println(temp1.getElemento());
+            temp1 = ll.after(temp1);
         }
     System.out.println(f.getElemento());
   }
