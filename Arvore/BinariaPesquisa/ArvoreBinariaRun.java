e0b1654d0a5723dd2eff867fe5e0b52a763f80a9
Carol <caroldazevedo52@gmail.com>
2025-11-25 13:35:31 -0300

    Create ArvoreBinariaRun.java

---
+++ Arvore/BinariaPesquisa/ArvoreBinariaRun.java
@@ -0,0 +1,19 @@
+public class ArvoreBinariaRun{
+    private static ArvoreBinaria arvore;
+
+    public static void main (String[] args){
+        arvore = new ArvoreBinaria(10);
+        Object temp;
+        int i;
+
+        //print
+
+        //Add
+        
+        //print
+
+        //remove
+        
+        //print
+    }
+}
